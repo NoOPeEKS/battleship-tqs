bacfa231203c0efa7052aaf4a329c4b9d575c459
Giro18 <101803263+Giro18@users.noreply.github.com>
2023-11-27 17:19:41 +0100

    Update MockBoard1.java

--- battleship/src/main/java/View/MockBoard1.java
+++ battleship/src/main/java/View/MockBoard1.java
@@ -5,21 +5,21 @@ public class MockBoard1 extends Board{
   public MockBoard1(){
     super();
 
-    //Barco de mira 3 en coord[0,0]
+    //Vaixell de mira 3 en coord[0,0]
     for (int col = 0; col < 2; col++){
       setShip(0, col, true);
     }
     
-    //Barco de mida 6 en coord[3,0]
+    //Vaixell de mida 6 en coord[3,0]
     for (int col = 0; col < 5; col++){
       setShip(3, col, true);
     }
-    //Barco de mida 4 en coord[1,0]
+    //Vaixell de mida 4 en coord[1,0]
     for (int col = 0; col < 3; col++){
       setShip(1, col, true);
     }
 
-    //Barco de mida 5 en coord[2,0]
+    //Vaixell de mida 5 en coord[2,0]
     for (int col = 0; col < 4; col++){
       setShip(2, col, true);
     }
